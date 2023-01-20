2dfb1ff32d102ea3d0497c479170a7ea10e12430
Omar Sharaan <O.Sharaan2@newcastle.ac.uk>
2023-01-20 11:26:25 +0000

    added favourite and remove favourite functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { Paper, TextInput, Button, Text, Group } from "@mantine/core";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const API_KEY = '3db70ceb3f7893404ff3c84127466fa5';
 
@@ -7,6 +7,18 @@ export default function Home() {
 
   const [city, setCity] = useState('');
   const [weatherData, setWeatherData] = useState<any>({});
+  const [favorites, setFavorites] = useState<string[]>([]);
+
+  useEffect(() => {
+    const storedFavorites = localStorage.getItem("favorites");
+    if (storedFavorites) {
+      setFavorites(JSON.parse(storedFavorites));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }, [favorites]);
 
   async function getWeatherData() {
     console.log(city);
@@ -21,6 +33,19 @@ export default function Home() {
       return alert('City not found');
     }
   }
+
+  function handleAddFavorite(city:string) {
+    setFavorites([...favorites, city]);
+  }
+
+  function handleRemoveFavorite(city:string) {
+    setFavorites(favorites.filter(fav => fav !== city));
+  }
+
+  function setCityAndFetchWeather(fav:string) {
+    setCity(fav);
+    getWeatherData();
+}
 
   return (
     <div
@@ -50,7 +75,7 @@ export default function Home() {
             Enter a city, and get the weather below!
           </Text>
         </Group>
-        <Group position='apart' mb='xs'>
+        <Group position='apart' grow mb='xs'>
           <TextInput
           label='City Name'
           icon={<img src='https://img.icons8.com/ios/50/000000/search--v1.png' width={20} height={20} />}
@@ -60,12 +85,21 @@ export default function Home() {
           />
         </Group>
         <Group position='apart'>
-          <Button size='md' variant='gradient' gradient={{ from: 'teal', to: 'blue' }} onClick={() => getWeatherData()}>
+          <Button size='sm' variant='gradient' gradient={{ from: 'teal', to: 'blue' }} onClick={() => getWeatherData()}>
             Get Weather
           </Button>
-          <Button size='md' variant='gradient' gradient={{ from: 'teal', to: 'blue' }} onClick={() => window.location.reload()}>
+          <Button size='sm' variant='gradient' gradient={{ from: 'teal', to: 'blue' }} onClick={() => window.location.reload()}>
             Reset Search
           </Button>
+          {favorites.includes(weatherData?.name) ? (
+            <Button size='sm' variant='gradient' gradient={{ from: 'teal', to: 'blue' }} onClick={() => handleRemoveFavorite(weatherData?.name)}>
+              Remove Favourite
+            </Button>
+          ) : (
+            <Button size='sm' variant='gradient' gradient={{ from: 'teal', to: 'blue' }} onClick={() => handleAddFavorite(weatherData?.name)}>
+              Add Favourite
+            </Button>
+          )}
         </Group>
         {weatherData?.main && (
           <>
@@ -105,6 +139,16 @@ export default function Home() {
                 </Text>
               </Text>
             </Group>
+                <Text size='xl' weight={500}>
+                  Favourite Cities
+                </Text>
+              <Group mt='xs'>
+                {favorites.map((fav:string) => (
+                  <Button key={fav} size='sm' variant='gradient' gradient={{ from: 'teal', to: 'blue' }} onClick={() => setCityAndFetchWeather(fav)}>
+                    {fav}
+                  </Button>
+                ))}
+              </Group>
           </>
         )}
       </Paper>
